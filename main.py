91bdd0733a75c6011a65506bbc5ad0a56b23198c
Gwen <gwenfaraday@pm.me>
2022-03-22 16:42:24 -0400

    adds setup for initial step

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ Step 2: Create a basic Transaction class.
 Step 3: Create helper functions to generate addresses and transactions.
 
 """
-from block import Block
-from helpers import generate_transaction
 
 
 def main():
     """
     Creates a block, populates it with random transactions, and prints it.
     """
-    block = Block()
-    for _ in range(5):
-        block.add_transaction(generate_transaction())
-    print(block)
+    ...
 
 
 if __name__ == "__main__":
